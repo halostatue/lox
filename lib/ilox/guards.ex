ac7d0eb6639b482135e08cb7605fc582fa0a94fa
Austin Ziegler <austin@zieglers.ca>
2024-11-03 16:14:41 -0500

    chore: Move guards into their own module

---
+++ lib/ilox/guards.ex
@@ -0,0 +1,13 @@
+defmodule Ilox.Guards do
+  @moduledoc false
+
+  defguard is_digit(c) when c in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+  @alpha String.split("_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", "", trim: true)
+
+  defguard is_alpha(c) when c in @alpha
+
+  defguard is_alphanumeric(c) when is_digit(c) or is_alpha(c)
+
+  defguard is_whitespace(c) when c in ["\r", "\s", "\t"]
+end
